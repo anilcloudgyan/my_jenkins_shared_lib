09727a0300a9550b83a69eb93ddde4a8cb49c547
anilcloudgyan <anil.cloudgyan@gmail.com>
2023-04-23 12:00:10 +0530

    changes chcekout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
-        branches: [[nameb:  stageParams.branch ]],
+        branchesAnil: [[name:  stageParams.branch ]],
         userRemoteConfigs: [[ url: stageParams.url ]]
     ])
   }
